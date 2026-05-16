fcd3c6f8f61963f25df14539e34051f73bd2fcef
mbalicki <mateusz.balick96@gmail.com>
2026-05-16 11:26:35 +0200

    reactivness and interactions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('mateusz.balicki@gmaill.com')
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let validationMessage = '';
+    if (email.length < 5) {
+        validationMessage = 'Email jest za krótki'
+    } else if (email.length < 15) {
+        validationMessage = 'Email jest odpowiedni'
+    } else {
+        validationMessage = 'Email jest za długi'
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <input type="text" id="femail" value={email} onChange={handleChange}/>
+            <button type ="button" onClick={() => alert(email)}>Wyświetl alert</button>
+            {email.length > 0 &&<div>Błąd: {validationMessage}</div>}
         </div>
     );
 }
